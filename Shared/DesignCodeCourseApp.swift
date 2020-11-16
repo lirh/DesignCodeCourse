facdd1f7a32aed1c15c5dcc1abf8e1527ef542e9
LiRH <li.rh@icloud.com>
2020-11-17 00:27:08 +0800

    first commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by 李瑞华 on 2020/11/17.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
